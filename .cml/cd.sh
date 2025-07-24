e24f9caf1ff70fa00dc54c7cb86a1fda5f85e8b8
parthiban222 <parthibane@outlook.com>
2025-07-24 17:48:15 +0000

    update 10

--- .cml/cd.sh
+++ .cml/cd.sh
@@ -11,7 +11,10 @@ echo "🚀 Pushing image to Artifact Registry..."
 docker push us-central1-docker.pkg.dev/ageless-aura-461314-a1/my-repo/iris-api:latest
 
 echo "♻️ Updating Kubernetes deployment image..."
-kubectl set image deployment/iris-workload iris-api-sha256-1=us-central1-docker.pkg.dev/ageless-aura-461314-a1/my-repo/iris-api:latest --namespace=default
+#kubectl set image deployment/iris-workload iris-api-sha256-1=us-central1-docker.pkg.dev/ageless-aura-461314-a1/my-repo/iris-api:latest --namespace=default
+gcloud container clusters get-credentials iris-kubernetes --zone us-central1 --project ageless-aura-461314-a1
+kubectl get podsi
+kubectl get service
 
 echo "✅ Deployment updated with new image!"
 
